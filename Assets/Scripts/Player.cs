a1b8a6228d0701d70cf69f05fceafe1da141e81c
ClMartin <clmartinpro1@gmail.com>
2025-09-10 21:46:14 +0200

    [ADD] 3D Model of cars and Car and wheel controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Rigidbody rb;
+
+    [Header("Inputs")]
+    [SerializeField] private InputActionReference move;
+
+    [Header("Speed")]
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private float maxVerticalSpeed;
+    [SerializeField] private float horizontalSpeed;
+
+    private Vector2 moveDirection = Vector2.zero;
+
+    private void Update()
+    {
+        moveDirection = move.action.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.AddForce(Vector3.right * moveDirection.y * verticalSpeed * Time.fixedDeltaTime,ForceMode.Force);
+        Vector3 velocity = Vector3.right * moveDirection.y * verticalSpeed;
+        rb.linearVelocity = new Vector3(velocity.x, rb.linearVelocity.y, velocity.z);
+
+
+    }
+}
