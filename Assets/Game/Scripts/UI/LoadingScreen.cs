660cf72dcfb2698daa9a033bb0d3ff40f5a34f30
ClMartin <clmartinpro1@gmail.com>
2025-10-16 20:59:44 +0200

    [OPTIMISATION] terrain, light texture [ADD] percentage to loading screen

--- Assets/Game/Scripts/UI/LoadingScreen.cs
+++ Assets/Game/Scripts/UI/LoadingScreen.cs
@@ -1,18 +1,26 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadingScreen : CustomScreen
 {
     [SerializeField] private Image backgroundImage;
+    [SerializeField] private TextMeshProUGUI txtPercentage;
 
     public void UpdateBackgroundImage(float percentage)
     {
         backgroundImage.fillAmount = percentage;
     }
 
+    public void UpdateTextPercetage(string percentage)
+    {
+        txtPercentage.text = percentage;
+    }
+
     public override void Show()
     {
         base.Show();
         UpdateBackgroundImage(1);
+        UpdateTextPercetage("0 %");
     }
 }
