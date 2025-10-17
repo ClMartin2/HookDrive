8741060b6c70cad6e2f7705191d6068957168e2e
ClMartin <clmartinpro1@gmail.com>
2025-10-17 17:01:58 +0200

    [ADD] Lithting settings when switching scene

--- Assets/Game/Scripts/Managers/Decor.cs
+++ Assets/Game/Scripts/Managers/Decor.cs
@@ -1,12 +1,33 @@
+using UnityEditor;
 using UnityEngine;
 
 public class Decor : MonoBehaviour
 {
     [SerializeField] private Material skyboxMaterial;
 
-    private void Start()
+    [Header("Lighting")]
+    [SerializeField] private LightingDataAsset lightingData;
+    [SerializeField] private UnityEngine.Rendering.AmbientMode ambientMode = UnityEngine.Rendering.AmbientMode.Skybox;
+    [SerializeField] private float ambientIntensity = 1f;
+    [SerializeField] private float reflectionIntensity = 1f;
+
+    private void OnEnable()
     {
         RenderSettings.skybox = skyboxMaterial;
-        //DynamicGI.UpdateEnvironment();
+        ApplyLightingSettings();
+    }
+
+    private void ApplyLightingSettings()
+    {
+        if (lightingData == null)
+            return;
+
+        Lightmapping.lightingDataAsset = lightingData;
+
+        RenderSettings.ambientMode = ambientMode;
+        RenderSettings.ambientIntensity = ambientIntensity;
+        RenderSettings.reflectionIntensity = reflectionIntensity;
+
+        DynamicGI.UpdateEnvironment();
     }
 }
